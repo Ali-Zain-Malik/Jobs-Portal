162fb1e58563a8cde1c7c77dbd196ae48ded0e34
Ali Zain Malik <alizainmalik123@gmail.com>
2024-09-24 17:07:32 +0500

    Redirect to home page

--- Jobs-4U/routes/web.php
+++ Jobs-4U/routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use App\Http\Controllers\user\AuthController;
+use App\Http\Middleware\userRedirect;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get("/signup", [AuthController::class, "index"])->name("user.signup");
-Route::post("/create-account", [AuthController::class, "createAccount"])->name("user.createAccount");
+Route::group(["middleware"=> userRedirect::class], function()
+{
+    Route::get("/signup", [AuthController::class, "index"])->name("user.signup");
+    Route::get("/signin", [AuthController::class, "signin"])->name("user.signin");
+    Route::post("/authenticate", [AuthController::class, "authenticate"])->name("user.authenticate");
+    Route::post("/create-account", [AuthController::class, "createAccount"])->name("user.createAccount");
+});
 
 
-Route::get("/signin", [AuthController::class, "signin"])->name("user.signin");
-Route::post("/authenticate", [AuthController::class, "authenticate"])->name("user.authenticate");
 
 
 Route::get("/home", function(){return view("user.home");})->name("user.home");
