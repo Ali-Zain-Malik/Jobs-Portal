f636d04a11f5fc68c0c9f4e30e261412e73d9910
Ali-Zain-StarsDev <alizainmalik123@gmail.com>
2024-10-10 12:29:13 +0500

    Admin sign in logic applied

---
+++ Jobs-4U/app/Http/Controllers/admin/AdminAuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuthController extends Controller
+{
+    public function index()
+    {
+        return view("admin.signin");
+    }
+
+    public function adminAuthenticate(Request $request)
+    {
+        $validator  =   Validator::make($request->all(),[
+            "email"     =>  "required|email",
+            "password"  =>  "required"
+        ]);
+
+        if($validator->fails())
+        {
+            return redirect()->route("admin.signin")->withErrors($validator);
+        }
+
+        $credentials    =   $request->only("email", "password");
+        if(Auth::guard("admin")->attempt($credentials))
+        {
+            if(Auth::guard("admin")->user()->role != "admin")
+            {
+                Auth::guard("admin")->logout();
+                return redirect()->route("admin.signin")->with("error", "You are not authorized to access this page.");
+            }
+            
+            return view("admin.dashboard");
+        }
+        else
+        {
+            return redirect()->route("admin.signin")->with("error", "Either email or password is incorrect!");
+        }
+    }
+}
